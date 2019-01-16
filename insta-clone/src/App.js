d399e275139448f5a028d92ba53b06e922c7ac99
Randy Wilson <phoenixrr2113@gmail.com>
2019-01-15 21:43:49 -0500

    searchbar done

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -11,13 +11,46 @@ import SearchBar from './components/SearchBar/search_bar';
 class App extends Component {
 	state = {
 		postData: dummyData,
+		searchTerm: '',
+	};
+
+	userSearch = event => {
+		event.preventDefault();
+
+		if (!this.state.searchTerm) {
+			this.setState({
+				postData: dummyData,
+			});
+		} else {
+			this.setState({
+				postData: this.state.postData.filter(post => {
+					return post.username === this.state.searchTerm;
+				}),
+			});
+		}
+	};
+
+	onInputChange = event => {
+		this.setState({ searchTerm: event.target.value });
 	};
 
 	render() {
 		return (
 			<MuiThemeProvider>
 				<div className="App">
-					<SearchBar posts={this.state.postData} />
+					<div className="header">
+						<div className="logo">
+							<i className="fab fa-instagram" />
+						</div>
+						<div className="search-bar">
+							<SearchBar
+								inputTerm={this.state.searchTerm}
+								handleChange={this.onInputChange}
+								userSearch={this.userSearch}
+							/>
+						</div>
+						<div className="profile-icons">ICONS</div>
+					</div>
 
 					{this.state.postData.map((el, i) => {
 						return <PostContainer posts={el} key={i} />;
--- insta-clone/src/components/SearchBar/search_bar.js
+++ insta-clone/src/components/SearchBar/search_bar.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import AutoComplete from 'material-ui/AutoComplete';
 
 const SearchBar = props => {
 	return (
-		<div>
-			<AutoComplete
-				floatingLabelText="Search"
-				filter={AutoComplete.fuzzyFilter}
-				dataSource={props.posts}
-				maxSearchResults={5}
-			/>
+		<div className="search-bar">
+			<form onSubmit={props.userSearch}>
+				<input value={props.inputTerm} onChange={props.handleChange} />
+			</form>
 		</div>
 	);
 };
